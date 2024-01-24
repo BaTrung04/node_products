1706d2239b185cf7ff18c81f57bcaebe9b491c36
BaTrung04 <batrung0410job@gmail.com>
2024-01-24 11:12:44 +0700

    Layout-admin-dashboard

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
+//route client-admin
 const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 
 database.connect();
 
@@ -13,11 +17,15 @@ const port = process.env.PORT;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+//App locals variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 //file tinh
 app.use(express.static("public"));
 
 //Route
 route(app);
+routeAdmin(app);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
